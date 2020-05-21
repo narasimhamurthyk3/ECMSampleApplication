995ee16054f0ec25edbe7eda8b9a6d0453424fb7
narasimhamurthyk3 <38559055+narasimhamurthyk3@users.noreply.github.com>
2020-05-21 16:22:45 +0530

    application v1
    application v1

--- src/main/java/com/ecm/rest/controller/WelcomeController.java
+++ src/main/java/com/ecm/rest/controller/WelcomeController.java
@@ -25,7 +25,7 @@ public class WelcomeController {
     
     @GetMapping("/welcome")
     @ResponseBody
-    public Welcome welcomeApp(@RequestParam(name="name", required=false, defaultValue="Application V2") String name) {
+    public Welcome welcomeApp(@RequestParam(name="name", required=false, defaultValue="Application V1") String name) {
         return new Welcome(String.format(applicationName, name));
     }
 
