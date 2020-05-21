05931e4dfce58cccf28955fbd742eed7986f732d
narasimhamurthyk3 <38559055+narasimhamurthyk3@users.noreply.github.com>
2020-05-21 14:20:12 +0530

    SourceCode

---
+++ src/main/java/com/ecm/rest/controller/WelcomeController.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.ecm.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ecm.beans.Welcome;
+
+@Controller
+public class WelcomeController {
+
+	private static final String welcomemsg = "Welcome Mr. %s!";
+	
+	private static final String applicationName = "Welcome to %s!";
+
+    @GetMapping("/welcome/user")
+    @ResponseBody
+    public Welcome welcomeUser(@RequestParam(name="name", required=false, defaultValue="Java Fan") String name) {
+        return new Welcome(String.format(welcomemsg, name));
+    }
+    
+    @GetMapping("/welcome")
+    @ResponseBody
+    public Welcome welcomeApp(@RequestParam(name="name", required=false, defaultValue="Application V2") String name) {
+        return new Welcome(String.format(applicationName, name));
+    }
+
+}
